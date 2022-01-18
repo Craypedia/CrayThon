b3bec3d07639e343a88dada836c5df31065681c2
Ridwan Abdulhameed <ridwanabdulhameed@gmail.com>
2022-01-18 15:51:56 +0100

    varriables, values, exoressions, operators

---
+++ first_class.py
@@ -0,0 +1,43 @@
+# print("Welcome To Python!")
+
+
+str = ('Are You Mad')
+int = (1999)
+float = (3.0)
+
+print(str)
+print(int)
+print(float)
+
+
+a = "This is a string"
+print(a)
+
+x = (-10 + (60 ** 0.5)) / 10
+print(x)
+
+
+
+bookprice = (24.95)
+discount = (0.4)
+total_cost = (3 * 42)
+
+whl_cost = (bookprice * discount * total_cost)
+print(whl_cost)
+
+
+price_60 = (24.95 * 60) * 0.6
+shipping = 3 + (0.75*59)
+total = (price_60 + shipping)
+print(total)
+
+
+
+area = 20
+carpert_cost = 24.99
+cushion_cost = 2.99
+labor_cost = 18
+
+total_cost = (carpert_cost * 20) + (cushion_cost * 20) + (labor_cost * 20) + (35)
+
+print (total_cost)
