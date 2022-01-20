cd2d5b9579d48b114007551ee4cbf68f35af350d
Ridwan Abdulhameed <ridwanabdulhameed@gmail.com>
2022-01-20 15:10:04 +0100

    strings, lists

---
+++ second_class.py
@@ -0,0 +1,88 @@
+# from operator import index
+# import string
+
+
+# a = 2
+# b = 2
+# print ("a" + "b")
+
+
+# my_string = "This is a string"
+# print(my_string)
+
+
+# my_string = "This is 90890 naira"
+# st2 = "This 59090 is lovely"
+
+# first_num = (my_string[8:12])
+# second_num = (st2[5:8])
+# print(int(first_num) + int(second_num))
+
+
+# added = int(first_num) + int(second_num)
+# print(f"The sum of {first_num} and {second_num} is {added}")
+# print(f"The sum of %s and %s is %d" % (first_num, second_num, added))
+
+
+
+# print("This is Ayo's book")
+# print("Dear Buhari,\nPlease don't run for President again\n\tYours in the country.\n\tAstroverse")
+
+
+# a = "aaboy"
+# print(a.index("a"))
+# print(a.rindex("a"))
+# print(a.count("a"))
+# print(a.replace("a","w"))
+
+# my_sen = "A quick brown fox jumps over a lazy dog"
+# a = my_sen.split()
+# print (a)
+
+# num = ['0','4','3','2']
+# otp = "".join(num)
+# print(otp)
+
+
+
+# s1 = "Ault"
+# s2 = "Kelly"print("original strings are", s1, s2)
+
+
+
+
+
+# str1 = "Welcome to USA, usa awesome, isn't it"
+# a = str1.lower().count("usa")
+# print(f"The USA count is {a}")
+
+
+
+# str1 = "Emma is a data scientist who knows Python. Emma works at Google"
+# index = str1.rfind("Emma")
+# print("Last occurrence of Emma starts at index:", index)
+
+# str1 = "Emma-is-a-data-scientist"
+# subb = str1.split("-")
+# sub = str1.replace("-"," ")
+# print(sub)
+
+
+
+
+# str1 = "/*Jon is @developer & musician"
+# rep = str1.replace("/*", "",).replace("@", "a ")
+# print(rep)
+
+
+
+# my_list = ["I", "am", "good"]
+# a_list = ["she", "is", "a", "queen"]
+
+# b = (my_list + a_list)
+
+# print(b[0: :2])
+
+
+list_a = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+print(list_a[2][2][0] + list_a[2][3])
